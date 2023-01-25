54a91563a34aea599c7a2c8e6630bf68c96d6087
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-25 21:35:09 +0200

    Detecting Mouse Input
    Clicking on tiles display messages in the console. Only the grass tiles are clickable so the player cannot place towers onto the road of the enemies.

--- RealmRush/Assets/Scripts/EnemyMover.cs
+++ RealmRush/Assets/Scripts/EnemyMover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] float secondsDelay = 2f;
+    [SerializeField][Range(0f, 5f)] float enemySpeed = 2f;
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,15 @@ public class EnemyMover : MonoBehaviour
     IEnumerator PrintWaypointName(){
         foreach (Waypoint waypoint in path)
         {
-            Vector3 waypointPosition = new Vector3(waypoint.transform.position.x,0,waypoint.transform.position.z);
-            transform.position = waypointPosition;
-            yield return new WaitForSeconds(secondsDelay);
+            Vector3 startPosition = transform.position;
+            Vector3 waypointPosition = new Vector3(waypoint.transform.position.x,3,waypoint.transform.position.z);
+            float travelPercent = 0f;
+            transform.LookAt(waypointPosition);
+            while(travelPercent < 1f){
+                transform.position = Vector3.Lerp(startPosition,waypointPosition,travelPercent);
+                travelPercent += Time.deltaTime * enemySpeed;
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
--- RealmRush/Assets/Scripts/Waypoint.cs
+++ RealmRush/Assets/Scripts/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] bool isNotPlaceable;
+   void OnMouseDown() {
+        if(isNotPlaceable){
+            return;
+        }
+        else{
+            Debug.Log(transform.name);
+        }
+   }
 }
